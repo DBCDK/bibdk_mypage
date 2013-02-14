7770de3aadf05239b350f99b74d073239c5de89b
JrgenGNielsen <jgn@dbc.dk>
2013-02-14 16:02:07 +0100

    alter user menu to point to bibdk_mypage

--- bibdk_mypage.api.php
+++ bibdk_mypage.api.php
@@ -16,9 +16,46 @@
  * @param $form
  * @param $form_state
  * @return array
- *  Array of search expressions
+ *  Render array
  */
-function hook_bibdk_mypage_tabs($form, $form_state) {
+function hook_bibdk_mypage_tabs($max_rows=3) {
+  global $user;
+
+  $form['module_name'] = array(
+    '#type'         => 'fieldset',
+    '#tree'         => TRUE,
+    '#theme'        => 'bibdk_mypage-form',
+    '#attributes'   => array('class' => array('element-wrapper', 'bibdk-mypage-wrapper')),
+  );
+
+  $form['module_name']['header'] = array(
+    '#type'           => 'markup',
+    '#markup'         => t('Account'),
+  );
+
+  $form['module_name']['rows'] = array(
+    '#type'         => 'fieldset',
+    '#tree'         => TRUE,
+  );
+
+  $form['module_name']['rows'][0]['label_row'] = array(
+    '#type'           => 'markup',
+    '#markup'         => t('Label'),
+  );
+  $form['module_name']['rows'][0]['value_row'] = array(
+    '#type'           => 'markup',
+    '#markup'         => t('Value'),
+  );
+
+  $form['module_name']['link_profile2_tab'] = array(
+    '#type'           => 'link',
+    '#title'          => t('Edit settings'),
+    '#title_display'  => 'invisible',
+    '#href'           => 'user/' . $user->uid . '/edit',
+    '#options'        => array('attributes' => array('title' => t('Edit settings'))),
+  );
+
+  return $form;
 }
 
 /**
