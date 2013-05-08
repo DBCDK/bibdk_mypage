f4ed97adc558d56a12d97fcf8510b3d7deceea4f
Martin Møller <mmj@dbc.dk>
2013-05-08 15:48:41 +0200

    No changes - code formatting only

--- bibdk_mypage.api.php
+++ bibdk_mypage.api.php
@@ -13,64 +13,64 @@
 /**
  * Get fieldsets from other modules.
  *
- * @param $form
- * @param $form_state
- * @return array
- *  Render array
+ * @param int $max_rows
+ * @internal param $form
+ * @internal param $form_state
+ * @return array Render array
  */
-function hook_bibdk_mypage_tabs($max_rows=3) {
+function hook_bibdk_mypage_tabs($max_rows = 3) {
 
   global $user;
 
   $form['module_name'] = array(
-    '#type'         => 'fieldset',
-    '#tree'         => TRUE,
-    '#theme'        => 'bibdk_mypage-form',
-    '#attributes'   => array('class' => array('element-wrapper', 'bibdk-mypage-wrapper')),
+    '#type' => 'fieldset',
+    '#tree' => TRUE,
+    '#theme' => 'bibdk_mypage-form',
+    '#attributes' => array('class' => array('element-wrapper', 'bibdk-mypage-wrapper')),
   );
 
   $form['module_name']['header'] = array(
-    '#type'           => 'markup',
-    '#markup'         => t('header'),
+    '#type' => 'markup',
+    '#markup' => t('header'),
   );
 
   $form['module_name']['rows'] = array(
-    '#type'         => 'fieldset',
-    '#tree'         => TRUE,
+    '#type' => 'fieldset',
+    '#tree' => TRUE,
   );
 
   $cart = array();
 
-  if ( sizeof($array) > 0 ) {
+  if (sizeof($array) > 0) {
     $array = array_slice($array, 0, $max_rows);
     foreach ($array as $id => $item) {
       $form['module_name']['rows'][$id] = array(
-        'label_row'  => array(
-                            '#type'     => 'markup',
-                            '#markup'   => $item->itemName,
-                           ),
-        'value_row'  => array(
-                            '#type'     => 'markup',
-                            '#markup'   => $item->itemValue,
-                           ),
+        'label_row' => array(
+          '#type' => 'markup',
+          '#markup' => $item->itemName,
+        ),
+        'value_row' => array(
+          '#type' => 'markup',
+          '#markup' => $item->itemValue,
+        ),
       );
     }
   }
   else {
     $form['module_name']['rows'][] = array(
-      'item_row'  => array(
-        '#type'     => 'markup',
-        '#markup'   => t('No items in module'),
-       ),
+      'item_row' => array(
+        '#type' => 'markup',
+        '#markup' => t('No items in module'),
+      ),
     );
   }
 
   $form['module_name']['link_profile2_tab'] = array(
-    '#type'           => 'link',
-    '#title'          => t('Go to tab'),
-    '#title_display'  => 'invisible',
-    '#href'           => 'user/' . $user->uid . '/edit/module_name_list',
-    '#options'        => array('attributes' => array('title' => t('Go to tab'))),
+    '#type' => 'link',
+    '#title' => t('Go to tab'),
+    '#title_display' => 'invisible',
+    '#href' => 'user/' . $user->uid . '/edit/module_name_list',
+    '#options' => array('attributes' => array('title' => t('Go to tab'))),
   );
 
   return $form;
